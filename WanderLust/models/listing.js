03b6914bc8c83fa211f3eb2c35276b2653e5878e
Shashank Soni <shashanksoni815@gmail.com>
2024-10-07 17:29:16 +0530

    title

--- WanderLust/models/listing.js
+++ WanderLust/models/listing.js
@@ -2,9 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title: String,
+    title: {
+        type: String,
+        require: true,
+    },
     description: String,
-    image: String,
+    image: {
+        type: String,
+        set: (v) => v === "" ? "default link" : v,
+    },
     price: Number,
     location: String,
     country: String,
